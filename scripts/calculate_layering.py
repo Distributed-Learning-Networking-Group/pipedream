cde45dcbf136157fb79495b78ffbaef7d02bc1b9
GITD245 <2456388197@qq.com>
2024-03-25 14:41:25 +0800

    calculate_layering

---
+++ scripts/calculate_layering.py
@@ -0,0 +1,218 @@
+import math
+import os
+import numpy
+import math
+import model_detail
+from torchvision.models import vgg16
+import torch.nn as nn
+# initial 动态规划目标矩阵layers stages replicate_time gpus
+w = numpy.zeros((200, 10, 10, 10))
+# transfer_channel = []  # perf 多机之间的带宽
+# per layer sum time compute f+b 序号从1开始
+# parameter_size = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # 每层的梯度大小
+process_time = []  # 前向计算与方向计算时间之和
+DNN_per_layer_compute_time_foward = []  # 每层的前向计算时间
+DNN_per_layer_compute_time_backward = []  # 每层的反向计算时间
+DNN_per_layer_activation = []  # 每层的激活量 前向给后向传的东西 out_put_size
+
+DNN_per_layer_gradients = []  # 每层更新时候的梯度 optimizer.step()呢部分的
+
+GPUS_order_list = [0, 0, 1, 2, 3]  # gpu之间带宽由高到低，里面应该是gpu的id
+GPUS_bandwidth = []  # 10Gbps to MByte
+
+
+PIPEDREAM_FLAG = 1
+GPU_NUM = 4
+DIR = "profile"
+DEP_IDX = 2
+
+
+def read_file(filename):
+    list = []
+    with open(DIR+'/'+filename) as file:
+        for line in file:
+            list.append(line)
+    return list
+
+
+def data_init(layers_detail):
+    file_names = os.listdir(DIR)
+
+    # init DNN_per_layer_compute_time
+    for file_index in range(0, len(file_names)):
+        if 'bac_.txt' in file_names[file_index]:
+            list = read_file(file_names[file_index])
+            for i in list:
+                DNN_per_layer_compute_time_backward.append(float(i))
+        elif 'for_.txt' in file_names[file_index]:
+            list = read_file(file_names[file_index])
+            for i in list:
+                DNN_per_layer_compute_time_foward.append(float(i))
+
+    # init process_time
+    for index in range(0, len(DNN_per_layer_compute_time_foward)):
+        process_time.append(
+            DNN_per_layer_compute_time_foward[index]+DNN_per_layer_compute_time_backward[index])
+
+    # init DNN_per_layer_activation
+    for index in range(0, len(layers_detail[DEP_IDX]['output_shape'])):
+        per_layer_shape = layers_detail[DEP_IDX]['output_shape'][index]
+        per_layer_size = math.prod(per_layer_shape) * \
+            32/8/1024/1024  # shape*float32 to MByte
+        DNN_per_layer_activation.append(per_layer_size)
+
+    # init DNN_per_layer_gradients
+    for index in range(0, len(layers_detail[DEP_IDX]['kernel_shape'])):
+        per_layer_gradients = layers_detail[DEP_IDX]['kernel_shape'][index]
+        per_layer_gradients_size = math.prod(per_layer_gradients) * \
+            32/8/1024/1024  # shape*float32 to MByte
+        DNN_per_layer_gradients.append(per_layer_gradients_size)
+
+    # init GPUS_bandwidth
+    for index in range(0, GPU_NUM):
+        GPUS_bandwidth.append([1280, 1280, 1280, 1280])
+
+
+def compute_all_reduce_time(ngpus, start_layer, end_layer, set_gpu):
+    if ngpus == 0:
+        return 0
+    sum_parameter = 0
+    # 看ddp交换的是什么，梯度还是参数
+    for i in range(start_layer, end_layer+1):
+        sum_parameter += DNN_per_layer_gradients[i]
+    mini_bandwidth = float('inf')
+    for index1, i in enumerate(set_gpu):
+        for index2, j in enumerate(set_gpu):
+            if GPUS_bandwidth[i][j] < mini_bandwidth:
+                mini_bandwidth = GPUS_bandwidth[i][j]
+    time = 2*(ngpus-1)*(sum_parameter)/(ngpus*mini_bandwidth)
+    return time
+
+
+def compute_communication_time(gpu_stage1, gpu_stage2, layer1_id):
+    mini_bandwidth = float('inf')
+    for index1, i in enumerate(gpu_stage1):
+        for index2, j in enumerate(gpu_stage2):
+            if GPUS_bandwidth[i][j] < mini_bandwidth:
+                mini_bandwidth = GPUS_bandwidth[i][j]
+    time = ((DNN_per_layer_activation[layer1_id]) /
+            (len(gpu_stage1)*len(gpu_stage2)*mini_bandwidth))
+    return time
+
+
+def compute_stage_partition(layers, n_gpus, stages, replicate_times, F_match={}, Sets=[]):
+    if layers < stages or n_gpus < stages:
+        return float('inf'), [], {}
+    if stages == 1 and replicate_times == n_gpus:
+        time_ = 0
+        for i in range(1, layers+1):
+            time_ += process_time[i]*(1/n_gpus)
+        time_ += compute_all_reduce_time(
+            n_gpus, 1, layers, GPUS_order_list[1:n_gpus+1]
+        )
+        w[layers][stages][replicate_times][n_gpus] = time_
+        Sets = list(range(1, layers+1))
+        F_match[tuple(Sets)] = tuple(GPUS_order_list[1:n_gpus+1])
+        # return w[layers][stages][replicate_times][n_gpus],Sets,F_match
+    else:
+        if stages == 1 or replicate_times == n_gpus:
+            return float('inf'), [], {}
+    min_max_time = float('inf')
+    if stages == 1 and replicate_times == n_gpus:
+        min_max_time = w[layers][stages][replicate_times][n_gpus]
+
+    F_match_ = {}
+    for L in range(1, layers):
+        for R in range(1, n_gpus-replicate_times+1):
+            w[L][stages-1][R][n_gpus-replicate_times], Sets_, F_match = compute_stage_partition(
+                L, n_gpus-replicate_times, stages-1, R)
+            communication_time = compute_communication_time(
+                GPUS_order_list[n_gpus-replicate_times-R+1:n_gpus-replicate_times +
+                                1], GPUS_order_list[n_gpus-replicate_times+1:n_gpus+1], L
+            )
+            process_times = 0
+            for i in range(L+1, layers+1):
+                process_times += process_time[i]*(1/replicate_times)
+            rest_time = process_times+compute_all_reduce_time(
+                replicate_times, L+1, layers, GPUS_order_list[n_gpus-replicate_times+1:n_gpus+1])
+            if communication_time < (1/2)*abs((rest_time-w[L][stages - 1][R][n_gpus - replicate_times])):
+                max_time = max(
+                    rest_time, w[L][stages - 1][R][n_gpus - replicate_times])
+            else:
+                if rest_time > w[L][stages-1][R][n_gpus-replicate_times]:
+                    max_time = max(rest_time, communication_time+rest_time-(1/2)
+                                   * (rest_time-w[L][stages - 1][R][n_gpus - replicate_times]))
+                else:
+                    max_time = max(w[L][stages - 1][R][n_gpus - replicate_times], communication_time + w[L][stages - 1][R][n_gpus - replicate_times] - (1 / 2) * (
+                        w[L][stages - 1][R][n_gpus - replicate_times] - rest_time))
+            if max_time < min_max_time:
+                min_max_time = max_time
+                s = list(range(L+1, layers+1))
+                Sets = [Sets_, s]
+                F_match[tuple(s)] = tuple(
+                    GPUS_order_list[n_gpus-replicate_times+1:n_gpus+1])
+                F_match_ = dict(F_match)
+    return min_max_time, Sets, F_match_
+
+
+def compute_stage_partition_pipedream(layers, n_gpus, stages, replicate_times, F_match={}, Sets=[]):
+    if layers < stages or n_gpus < stages:
+        return float('inf'), [], {}
+    if stages == 1 and replicate_times == n_gpus:
+        time_ = 0
+        for i in range(1, layers+1):
+            time_ += process_time[i]*(1/n_gpus)
+        time_ += compute_all_reduce_time(
+            n_gpus, 1, layers, GPUS_order_list[1:n_gpus+1]
+        )
+        w[layers][stages][replicate_times][n_gpus] = time_
+        Sets = list(range(1, layers+1))
+        F_match[tuple(Sets)] = tuple(GPUS_order_list[1:n_gpus+1])
+        # return w[layers][stages][replicate_times][n_gpus],Sets,F_match
+    else:
+        if stages == 1 or replicate_times == n_gpus:
+            return float('inf'), [], {}
+    min_max_time = float('inf')
+    if stages == 1 and replicate_times == n_gpus:
+        min_max_time = w[layers][stages][replicate_times][n_gpus]
+
+    F_match_ = {}
+    for L in range(1, layers):
+        for R in range(1, n_gpus-replicate_times+1):
+            w[L][stages-1][R][n_gpus-replicate_times], Sets_, F_match = compute_stage_partition(
+                L, n_gpus-replicate_times, stages-1, R)
+            communication_time = compute_communication_time(
+                GPUS_order_list[n_gpus-replicate_times-R+1:n_gpus-replicate_times +
+                                1], GPUS_order_list[n_gpus-replicate_times+1:n_gpus+1], L
+            )
+            process_times = 0
+            for i in range(L+1, layers+1):
+                process_times += process_time[i]*(1/replicate_times)
+            rest_time = process_times+compute_all_reduce_time(
+                replicate_times, L+1, layers, GPUS_order_list[n_gpus-replicate_times+1:n_gpus+1])
+            max_time = max(rest_time, 2*communication_time,
+                           w[L][stages - 1][R][n_gpus - replicate_times])
+
+            if max_time < min_max_time:
+                min_max_time = max_time
+                s = list(range(L+1, layers+1))
+                Sets = [Sets_, s]
+                F_match[tuple(s)] = tuple(
+                    GPUS_order_list[n_gpus-replicate_times+1:n_gpus+1])
+                F_match_ = dict(F_match)
+    return min_max_time, Sets, F_match_
+
+
+if __name__ == '__main__':
+    # w(layers, sum_gpus, stage_num, replicate_times)
+    layers_detail = model_detail.model_info(
+        vgg16(pretrained=False), batch_size=16).layers_info
+    data_init(layers_detail)
+
+    for i in range(1, 1+GPU_NUM):
+        for j in range(1, 1+GPU_NUM):
+            time, sets, match = compute_stage_partition(10, 4, i, j, {}, [])
+            print(i, j)
+            print(time)
+            print(sets)
+            print(match)
---
+++ scripts/model_detail.py
@@ -0,0 +1,100 @@
+from torchvision.models import vgg16
+from torchsummary import summary  # pip install torch-summary
+import torch.nn as nn
+import re
+test_model = vgg16(pretrained=False)
+
+
+class model_info():
+    """
+    Atributtes:
+        model:需要分析的模型
+        batch_size:训练时选用的batch_sizes
+
+    model_info.layers_info[summary中的depth-idx][具体参数]
+    """
+
+    def __init__(self, model: nn.modules, batch_size: int):
+        self.batch_size = batch_size
+        self.info = summary(model, (3, 224, 224), col_names=[
+                            "input_size", "output_size", "num_params", "kernel_size", "mult_adds"])
+        self.layers_info = []
+        for _ in range(0, 4):
+            self.layers_info.append({'layer_name': [],
+                                    'input_shape': [],
+                                     'output_shape': [],
+                                     'param': [],
+                                     'kernel_shape': [],
+                                     'mult_adds': [],
+                                     'total_params': int,
+                                     'trainable_params': int,
+                                     'non_trainable_params': int,
+                                     'total_mult_adds(G)': float,
+                                     'input_size(MB)': float,
+                                     'for/bac_pass_size(MB)': float,
+                                     'params_size(MB)': float,
+                                     'estimated_total_size(MB)': float})
+        self._info_split()
+
+    def _str_list_to_list(self, str_list):
+        if str_list == '--':
+            return [0]
+        list = str_list[1:-1].split(', ')
+        list = [int(i) if int(i) != -1 else self.batch_size for i in list]
+        return list
+
+    def _info_split(self):
+        layers = str(self.info).split('\n')
+        layers[0] = ''
+        layers[1] = ''
+        layers[2] = ''
+        layers[-1] = ''
+        layers[-6] = ''
+        layers[-11] = ''
+
+        for index in range(3, len(layers)-11):
+            # 根据depth-idx区分layer类型
+            dep_idx = int(re.search(r':\s*([^\s])', layers[index]).group(1))
+
+            layers[index] = re.split(r' {2,}', layers[index].split('─')[1])
+
+            self.layers_info[dep_idx]['layer_name'].append(layers[index][0])
+            self.layers_info[dep_idx]['input_shape'].append(
+                self._str_list_to_list(layers[index][1]))
+            self.layers_info[dep_idx]['output_shape'].append(
+                self._str_list_to_list(layers[index][2]))
+            if layers[index][3] == '--':
+                self.layers_info[dep_idx]['param'].append(0)
+            else:
+                self.layers_info[dep_idx]['param'].append(
+                    int(layers[index][3].replace(',', "")))
+            self.layers_info[dep_idx]['kernel_shape'].append(
+                self._str_list_to_list(layers[index][4]))
+            if layers[index][5] == '--':
+                self.layers_info[dep_idx]['mult_adds'].append(0)
+            else:
+                self.layers_info[dep_idx]['mult_adds'].append(
+                    int(layers[index][5].replace(',', "")))
+
+        self.layers_info[dep_idx]['total_params'] = int(
+            layers[-10].split()[-1].replace(',', ""))
+        self.layers_info[dep_idx]['trainable_params'] = int(
+            layers[-9].split()[-1].replace(',', ""))
+        self.layers_info[dep_idx]['non_trainable_params'] = int(
+            layers[-8].split()[-1].replace(',', ""))
+        self.layers_info[dep_idx]['total_mult_adds(G)'] = float(
+            layers[-7].split()[-1])
+
+        self.layers_info[dep_idx]['input_size(MB)'] = float(
+            layers[-5].split()[-1])
+        self.layers_info[dep_idx]['for/bac_pass_size(MB)'] = float(
+            layers[-4].split()[-1])
+        self.layers_info[dep_idx]['params_size(MB)'] = float(
+            layers[-3].split()[-1])
+        self.layers_info[dep_idx]['estimated_total_size(MB)'] = float(
+            layers[-2].split()[-1])
+
+
+# if __name__ == '__main__':
+#     layers_info = model_info(test_model, batch_size=16)
+#     print(layers_info.layer_info[2]['output_shape'])
